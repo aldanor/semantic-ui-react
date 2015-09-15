5ef99c5729d2cb62062a384834c4b37c2db5f814
Ivan Smirnov <i.s.smirnov@gmail.com>
2015-09-16 00:11:04 +0100

    Optimize defineClassnames a bit

--- src/utils/props.js
+++ src/utils/props.js
@@ -7,20 +7,16 @@ function defineClassNames(component, props) {
         let classes = base ? [base] : [];
         for (const prop of props) {
             const value = this.props[prop.name];
-            if (prop.type == 'bool') {
-                classes.push({[prop.name]: value});
-            } else if (prop.type == 'string') {
-                if (value) {
+            if (value) {
+                if (prop.type == 'bool') {
+                    classes.push({[prop.name]: value});
+                } else if (prop.type == 'string') {
                     classes.push(value);
-                }
-            } else if (prop.type == 'oneOf') {
-                if (value) {
-                    if (!prop.compact) {
-                        classes.push(prop.name);
-                    }
-                    if (!prop.default || value != prop.default) {
-                        classes.push(value);
-                    }
+                } else if (prop.type == 'oneOf') {
+                    classes.push({
+                        [prop.name]: !prop.compact,
+                        [value]: !prop.default || value != prop.default
+                    });
                 }
             }
         }
